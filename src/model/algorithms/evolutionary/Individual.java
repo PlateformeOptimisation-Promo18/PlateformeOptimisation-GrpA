2a38766a65263918ea6627873c688c44bec87c9c
Lucas <lucas@paumel.com>
2020-04-01 20:07:38 +0200

    Platform skeleton 2
    Add others classes

---
+++ src/model/algorithms/evolutionary/Individual.java
@@ -0,0 +1,21 @@
+package model.algorithms.evolutionary;
+
+import model.generic.Solution;
+
+public class Individual extends Solution {
+	
+	private double force;
+	// etc ...
+
+	public Individual(int nObjectifs) {
+		super(nObjectifs);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void evaluer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
