8c489ca2c865f36ec5aa66a119536a999d086549
Paul Pitiot <p.pitiot@aveyron.cci.fr>
2020-04-01 15:44:17 +0200

    mise à jour de la classe scenario avec la méthode d'évaluation temporaire pour les tests mise à jour de la classe solution -> elle est abstraite + elle a des attributs -> list<Objectif> et int[] valuesVariables qui correspond aux valeurs des variables pour la solution

--- src/model/generic/Solution.java
+++ src/model/generic/Solution.java
@@ -1,11 +1,40 @@
 package model.generic;
 
-public class Solution {
+import java.util.List;
+
+public abstract class Solution {
 	
-	private int[] objectifs;
+	// j'ai modifié votre solution en classe abstraite 
+	// elle sera concrétisé par une classe lié au problème
+	// pour nous c'est la classe scénario
 	
-	public Solution (int nObjectifs) {
-		this.objectifs = new int[nObjectifs];
+	protected int [] valuesVariables;
+	protected List<Double> valuesObjectives;
+
+	public int[] getValuesVariables() {
+		return valuesVariables;
 	}
 
+	public List<Double> getValuesObjectives() {
+		return valuesObjectives;
+	}
+	
+	public Double getValueObjective(int i) {
+		return valuesObjectives.get(i);
+	}
+
+	public void setValuesVariables(int[] valuesVariables) {
+		this.valuesVariables = valuesVariables;
+	}
+	
+	public void setValuesVariables(int iIndexVariable, int iValue) {
+		this.valuesVariables[iIndexVariable] = iValue;
+		
+	}
+	// vous n'avez pas de classe problème !
+	
+	// public abstract void evaluate(Problem pb);
+
+
+	
 }
