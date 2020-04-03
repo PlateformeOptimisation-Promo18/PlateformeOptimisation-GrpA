74717801b8a3a1d1d98bc5af0cbf8248beb191dd
Lucas <lucas@paumel.com>
2020-04-03 12:27:49 +0200

    Init Algo

--- src/model/algorithms/ants/AntsColonyAlgorithm.java
+++ src/model/algorithms/ants/AntsColonyAlgorithm.java
@@ -5,15 +5,40 @@ import java.util.List;
 
 public class AntsColonyAlgorithm {
 	
-	private int[] noeuds;
-	private PheromonesTrails pheromonesTrails;
+	private List<Noeud> noeuds;
 	
-	public AntsColonyAlgorithm (int[] ListTabNoeud) {
-		this.noeuds = ListTabNoeud;
-		this.pheromonesTrails = new PheromonesTrails(ListTabNoeud,1000);
+	
+	public AntsColonyAlgorithm (int[] tabNoeuds) {
+		this.noeuds = new ArrayList<>();
+		for (int n : tabNoeuds) {
+			this.noeuds.add(new Noeud (n,this.getProbaNoeud(n)));
+		}
 	}
 	
+	private double getProbaNoeud (int n) {
+		return (double)Math.round(1.0/n * 100) / 100;
+	}
 	
+	public List<Noeud> getList () {
+		return this.noeuds;
+	}
 	
+	public class Noeud {
+		private int nbChoix;
+		private double proba;
+		public Noeud (int n, double p) {
+			this.nbChoix = n;
+			this.proba = p;
+		}
+		public int getNbChoice () {
+			return this.nbChoix;
+		}
+		public double getProba () {
+			return this.proba;
+		}
+		public String toString () {
+			return getNbChoice()+"-"+getProba();
+		}
+	}
 
 }
