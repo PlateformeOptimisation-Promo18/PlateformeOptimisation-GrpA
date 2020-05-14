2ffa37a0553931b345a6bd6a01a6063163014401
Lucas3IL18 <62331727+Lucas3IL18@users.noreply.github.com>
2020-05-14 10:42:22 +0200

    Creation class Problem

--- src/model/generic/Problem.java
+++ src/model/generic/Problem.java
@@ -1,13 +1,23 @@
 package model.generic;
 
-import java.io.File;
-
-public class Problem {
+public interface Problem {
 	
-	private File filePb;
-	
-	public Problem () {
-		
-	}
+	public Solution getSolution();
 
+	public Solution copySolution(Solution sol);
+	
+	public int [] getTabSizeDomainVariables();
+
+	public Double getMaxObjectif(int i);
+
+	public Double getMinObjectif(int i);
+
+	public int getNbObjectives();
+
+	public int getNbVariables();
+
+	public boolean[] GetActiveVariable(Solution sol);
+
+	public int getTabSizeDomainVariable(int i) throws Exception;
+	
 }
