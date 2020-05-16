bd5daefd1824d22cd6286eb50e96a76fa1cf808e
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-05-16 15:09:37 +0200

    [ADD] EndOrNode class

---
+++ src/model/problem/EndOrNode.java
@@ -0,0 +1,25 @@
+package model.problem;
+
+import java.util.Scanner;
+
+public class EndOrNode extends Node {
+
+    protected int iIdBeginOrNode;
+
+    public EndOrNode(Scanner sc) {
+//        TODO
+    }
+
+    public String toString() {
+//        TODO
+    }
+
+    protected void setiIdBeginOrNode(int iIdBeginOrNode) {
+        this.iIdBeginOrNode = iIdBeginOrNode;
+    }
+
+    protected int getiIdBeginOrNode() {
+        return this.iIdBeginOrNode;
+    }
+
+}
