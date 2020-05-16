c39a85a3b57cc9d554d193ce3cfd5d74c95f45cf
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-05-16 15:08:50 +0200

    [ADD] EndAndNode class

---
+++ src/model/problem/EndAndNode.java
@@ -0,0 +1,25 @@
+package model.problem;
+
+import java.util.Scanner;
+
+public class EndAndNode extends Node {
+
+    protected int iIdBeginAndNode;
+
+    public EndAndNode(Scanner sc) {
+//        TODO
+    }
+
+    public String toString() {
+//        TODO
+    }
+
+    protected void setiIdBeginAndNode(int iIdBeginAndNode) {
+        this.iIdBeginAndNode = iIdBeginAndNode;
+    }
+
+    protected int getiIdBeginAndNode() {
+        return this.iIdBeginAndNode;
+    }
+
+}
