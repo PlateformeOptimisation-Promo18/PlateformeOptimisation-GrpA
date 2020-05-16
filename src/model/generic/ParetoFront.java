b786a936e1a708281725843bf77d75cd503ad9ca
kenguyen9 <kenlee.nguyen1@gmail.com>
2020-05-16 08:33:36 +0200

    ParetoFront => Reduc by clusters

--- src/model/generic/ParetoFront.java
+++ src/model/generic/ParetoFront.java
@@ -61,18 +61,48 @@ public class ParetoFront {
 	}
 
 	
+	/**
+	 * Reduction du front de Pareto
+	 * @param nbMaxSol le nombre de solutions que l'on veut garder
+	 * @param pb 
+	 */
 	public void reduceIfNecessary(int nbMaxSol, Problem pb) {
-		
 	}
 	
+	/**
+	 * Calcule la distance entre sol1 et sol2 en fonction du nombre d'objectif
+	 * @param sol1 Solution 1
+	 * @param sol2 Solution 2
+	 * @param pb 
+	 * @return La distance calculée entre les deux solutions 
+	 */
 	private double getDistance(Solution sol1, Solution sol2, Problem pb) {
-		return 0.0;
+		int iNbObjectif = pb.getNbObjectives();
+		double powRes = 0;
+		for(int i=0;i<iNbObjectif;i++) {
+			powRes += Math.pow(Math.abs(sol2.getValueObjective(i) - sol1.getValueObjective(i)),2);
+		}
+		double res = Math.sqrt(powRes);
+		return res;
 	}
 	
+	/**
+	 * Calcule la distance entre cluster1 et cluster2 en fonction du nombre d'objectif
+	 * @param cluster1
+	 * @param cluster2
+	 * @param pb
+	 * @return La distance calculée entre les deux clusters
+	 */
 	private double getDistanceClusters(ArrayList<Solution> cluster1, ArrayList<Solution> cluster2, Problem pb) {
 		return 0.0;
 	}
 	
+	/**
+	 * Retourne la solution qui se situe le plus au centre d'un cluster
+	 * @param cluster 
+	 * @param pb
+	 * @return la solution qui se situe le plus au centre d'un cluster
+	 */
 	private Solution centroide(ArrayList<Solution> cluster, Problem pb) {
 		return null;
 	}
