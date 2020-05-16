44ab1a552fd24ec8677df05d51263f9bd62487ac
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-05-16 15:08:35 +0200

    [ADD] AndNode class

---
+++ src/model/problem/AndNode.java
@@ -0,0 +1,25 @@
+package model.problem;
+
+import java.util.Scanner;
+
+public class AndNode extends Node {
+
+    protected int iIdEndAndNode;
+
+    public AndNode(Scanner sc) {
+//        TODO
+    }
+
+    public String toString() {
+//        TODO
+    }
+
+    protected void setiIdEndAndNode(int iIdEndAndNode) {
+        this.iIdEndAndNode = iIdEndAndNode;
+    }
+
+    protected int getiIdEndAndNode() {
+        return this.iIdEndAndNode;
+    }
+
+}
