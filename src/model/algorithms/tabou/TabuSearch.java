df9edd2cea0b2ac3702c5d3d13a03d6f0af0ac1c
Léa <lea.batllefont@viacesi.fr>
2020-05-17 14:02:36 +0200

    Final version of tabuSearch + javadoc

--- src/model/algorithms/tabou/TabuSearch.java
+++ src/model/algorithms/tabou/TabuSearch.java
@@ -1,5 +1,6 @@
 package model.algorithms.tabou;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -7,18 +8,34 @@ import java.util.Set;
 
 import application.StopRequired;
 import model.generic.CombinatorialMultiObjectiveOptimizationAlgorithm;
+import model.generic.InterfaceRandom;
 import model.generic.Parameter;
 import model.generic.Problem;
 import model.generic.Solution;
-import utils.InterfaceRandom;
 
+/**
+ * The class TabuSearch is used to search for the best solution for a problem
+ * thanks to the tabou algorithm.
+ * 
+ * @author Léa BATLLE-FONT
+ *
+ */
 public class TabuSearch extends CombinatorialMultiObjectiveOptimizationAlgorithm{
+	//Default number of neighbours for a solution
 	private static final int NB_NEIGHBOURS = 10;
+	//Default number of iteration of the algorithm
 	private static final int NB_IERATION_MAX = 100;
-	private static final int SIZE_TABOU_LIST = 100;
+	//Default size of the tabou list, which is used as our memory to store every solutions evaluated
+	private static final int SIZE_TABOU_LIST = 1000;
 
-	public TabuSearch(StopRequired stop) {
-		super(stop);
+	/**
+	 * Constructeur of the TabuSearch algorithm. It sets the parameters required.
+	 * @param pb
+	 * @param stop This is a boolean used by the user to stop the algorithm.
+	 * @param algorithmName
+	 */
+	public TabuSearch(Problem pb,StopRequired stop, String algorithmName) {
+		super(pb, stop, "Tabou");
 
 		Parameter neighbours = new Parameter(NB_NEIGHBOURS, "Number of neighbours for a Solution");
 		this.listParam.add(neighbours);
@@ -30,7 +47,7 @@ public class TabuSearch extends CombinatorialMultiObjectiveOptimizationAlgorithm
 	}
 
 	@Override
-	public void launch(Problem pb, InterfaceRandom generator) {
+	public void launch(InterfaceRandom generator) {
 		/**
 		 * This list will store every solution we already checked to avoid infinite loop 
 		 * going throw the same solution over and over. It will be our memory.
@@ -44,6 +61,7 @@ public class TabuSearch extends CombinatorialMultiObjectiveOptimizationAlgorithm
 			e1.printStackTrace();
 		}
 
+		//Main loop who checked if the search continues or must stop.
 		while(count <= this.listParam.get(1).getValue().intValue() && !this.stopRequired 
 				&& tabouList.size() < this.listParam.get(2).getValue().intValue()) {
 
@@ -51,13 +69,11 @@ public class TabuSearch extends CombinatorialMultiObjectiveOptimizationAlgorithm
 			count++;
 			long startTime = System.nanoTime();
 			currentSolution.evaluate(pb);
-			this.evolutionTime.add((double) ((System.nanoTime() - startTime)/1000000));
 			this.evolutionHypervolum.add(currentSolution.evaluatePerf(pb));
 			this.bestSolutions.addSolutionIfIsParetoFrontSolution(currentSolution);
 			tabouList.add(currentSolution);
 
 			int neighbourCount = 0;
-			List<Solution> neighbourList = new ArrayList<>();
 			Solution bestNeighbour = currentSolution;
 			
 			//Then, creation of his neighbours and we evaluate them
@@ -65,6 +81,8 @@ public class TabuSearch extends CombinatorialMultiObjectiveOptimizationAlgorithm
 				neighbourCount++;
 				int[] tabPossibleValues = pb.getTabSizeDomainVariables();
 				int variableToChange = generator.nextInt(currentSolution.getNbVariables());
+				
+				//We duplicate the current solution so we can change it's value 
 				Solution neighbour = pb.copySolution(currentSolution);
 				neighbour.setValuesVariables(variableToChange, generator.nextInt(tabPossibleValues[variableToChange]));
 				
@@ -73,15 +91,26 @@ public class TabuSearch extends CombinatorialMultiObjectiveOptimizationAlgorithm
 					neighbour.setValuesVariables(variableToChange, generator.nextInt(tabPossibleValues[variableToChange]));
 				}
 				
+				//The new neighbour is added to the memory
 				tabouList.add(neighbour);
-				neighbourList.add(neighbour);
 				
+				//We check if this neighbour is better than the previous
 				if(neighbour.evaluatePerf(pb) > bestNeighbour.evaluatePerf(pb)) {
 					bestNeighbour = neighbour;
 				}
 			}
-			
+			long estimatedTime = (System.nanoTime() - startTime)/1000000;
+			this.evolutionTime.add(estimatedTime);
+
+			//The bestNeighbour becomes the currentSolution so we can look for it's best neighbour
 			currentSolution = bestNeighbour;
+			List<Solution> newSolution = new ArrayList<Solution>();
+			newSolution.add(currentSolution);
+			try {
+				updateAndSave(newSolution, estimatedTime);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
 		}
 
---
+++ src/test/model/algorithms/tabou/TabuSearchTest.java
@@ -0,0 +1,25 @@
+package test.model.algorithms.tabou;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.generic.Problem;
+import model.problem.GraphProject;
+
+public class TabuSearchTest {
+	
+	static GraphProject gp;
+
+	@Before
+	public void setUp() throws Exception {
+		gp = new GraphProject("Graph test");
+	}
+
+	@Test
+	public void testLaunch() {
+		fail("Not yet implemented");
+	}
+
+}
