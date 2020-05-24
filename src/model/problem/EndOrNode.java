8fc0e3e3c0a5de6b4e145869fb8b3ff4c2629d66
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-05-24 21:20:51 +0200

    [IMP] end of EndOrNode class

--- src/model/problem/EndOrNode.java
+++ src/model/problem/EndOrNode.java
@@ -2,22 +2,41 @@ package model.problem;
 
 import java.util.Scanner;
 
+/**
+ * Classe EndOrNode
+ * @author : Alexis V.
+ * @version : 1.0
+ */
 public class EndOrNode extends Node {
 
     protected int iIdBeginOrNode;
 
+    /**
+     * Permet de créer un objet avec des valeurs transmises en parametres
+     * @param sc : Scanner qui contient les valeurs qu'on souhaite renseigner lors de la creation de l'objet
+     */
     public EndOrNode(Scanner sc) {
-//        TODO
+        this.iIdBeginOrNode = sc.nextInt();
     }
 
+    /**
+     * @return : chaine de caractere qui decrit l'objet courant
+     */
     public String toString() {
-//        TODO
+        return String.format("Objet EndOrNode, iIdBeginOrNode : %d", this.iIdBeginOrNode);
     }
 
+    /**
+     * Permet de renseigner la valeur de iIdBeginOrNode
+     * @param iIdBeginOrNode : valeur qu'on souhaite affecter à iIdBeginOrNode sur l'objet courant
+     */
     protected void setiIdBeginOrNode(int iIdBeginOrNode) {
         this.iIdBeginOrNode = iIdBeginOrNode;
     }
 
+    /**
+     * @return : valeur de iIdBeginOrNode sur l'objet courant
+     */
     protected int getiIdBeginOrNode() {
         return this.iIdBeginOrNode;
     }
