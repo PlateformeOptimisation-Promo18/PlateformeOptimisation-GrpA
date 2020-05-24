0e1cc2e9ede422f7ea6b4114f04868ccdc6852e4
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-05-24 21:23:05 +0200

    [IMP] end of AndNode class

--- src/model/problem/AndNode.java
+++ src/model/problem/AndNode.java
@@ -2,22 +2,41 @@ package model.problem;
 
 import java.util.Scanner;
 
+/**
+ * Classe AndNode
+ * @author : Alexis V.
+ * @version : 1.0
+ */
 public class AndNode extends Node {
 
     protected int iIdEndAndNode;
 
+    /**
+     * Permet de créer un objet avec des valeurs transmises en parametres
+     * @param sc : Scanner qui contient les valeurs qu'on souhaite renseigner lors de la creation de l'objet
+     */
     public AndNode(Scanner sc) {
-//        TODO
+        this.iIdEndAndNode = sc.nextInt();
     }
 
+    /**
+     * @return : chaine de caractere qui decrit l'objet courant
+     */
     public String toString() {
-//        TODO
+        return String.format("Objet AndNode, iIdEndAndNode : %d", this.iIdEndAndNode);
     }
 
+    /**
+     * Permet de renseigner la valeur de iIdEndAndNode
+     * @param iIdEndAndNode : valeur qu'on souhaite affecter à iIdEndAndNode sur l'objet courant
+     */
     protected void setiIdEndAndNode(int iIdEndAndNode) {
         this.iIdEndAndNode = iIdEndAndNode;
     }
 
+    /**
+     * @return : valeur de iIdEndAndNode sur l'objet courant
+     */
     protected int getiIdEndAndNode() {
         return this.iIdEndAndNode;
     }
