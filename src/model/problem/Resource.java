5d0c79ef47a3856e6ba73a26b1065166a8362c34
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-05-27 21:43:18 +0200

    [IMP] ressource class

--- src/model/problem/Resource.java
+++ src/model/problem/Resource.java
@@ -2,43 +2,78 @@ package model.problem;
 
 import java.util.Scanner;
 
+/**
+ * Classe Resource
+ * @author : Alexis V.
+ * @version : 1.0
+ */
 public class Resource implements Cloneable {
 
     protected int iIdResource;
     protected double dQuantity;
 
+    /**
+     * Permet de créer un objet avec des valeurs transmises en parametres
+     * @param sc : Scanner qui contient les valeurs qu'on souhaite renseigner lors de la creation de l'objet
+     */
     public Resource(Scanner sc) {
-//        TODO
+        this.iIdResource = sc.nextInt();
+        this.dQuantity = sc.nextDouble();
     }
 
+    /**
+     * Constructeur surchargé avec un objet du même type
+     * @param resource : Objet ressource avec lequel on souhaite en créer un nouveau
+     */
     public Resource(Resource resource) {
-//        TODO
+        this.iIdResource = resource.iIdResource;
+        this.dQuantity = resource.dQuantity;
     }
 
     public Resource(Scanner sc, GraphProject gp) throws Exception {
 //        TODO
     }
 
+    /**
+     * @return : chaine de caractere qui decrit l'objet courant
+     */
     public String toString() {
-//        TODO
+        return "Objet Ressource -> iIdResource : " + this.iIdResource + " / dQuantity : " + this.dQuantity;
     }
 
+    /**
+     * Permet de soustraire une quantité sur l'attribut dQuantity
+     * @param dQuantity : quantité qu'on souhaite soustraire à dQuantity sur l'objet courant
+     */
     public void removeQuantity(double dQuantity) {
-//        TODO
+        this.dQuantity -= dQuantity;
     }
 
+    /**
+     * Permet d'ajouter une quantité sur l'attribut dQuantity
+     * @param dQuantity : quantité qu'on souhaite ajouter à dQuantity sur l'objet courant
+     */
     public void addQuantity(double dQuantity) {
-//        TODO
+        this.dQuantity += dQuantity;
     }
 
+    /**
+     * @return : une copy de l'objet de l'objet courant
+     */
     protected Resource clone() {
-//        TODO
+        return new Resource(this);
     }
 
+    /**
+     * @return : valeur de iIdResource sur l'objet courant
+     */
     public int getiIdResource() {
         return this.iIdResource;
     }
 
+    /**
+     * @return : valeur de iIdOrNode sur l'objet courant
+     */
     public double getdQuantity() {
         return this.dQuantity;
     }
