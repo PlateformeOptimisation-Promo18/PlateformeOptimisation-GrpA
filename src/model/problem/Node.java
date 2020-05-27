023bed505d59d37e761500da11ac8f58c6fb4ae7
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-05-27 21:28:24 +0200

    [IMP] node class

--- src/model/problem/Node.java
+++ src/model/problem/Node.java
@@ -1,57 +1,105 @@
 package model.problem;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe abstraite Node
+ * @author : Alexis V.
+ * @version : 1.0
+ */
 public abstract class Node implements Comparable<Node> {
 
     protected int iIdNode;
-    List<Integer> listNexts;
-    List<Integer> listPrevious;
+    protected List<Integer> listNexts;
+    protected List<Integer> listPrevious;
 
+    /**
+     * Création d'un Node avec listNexts et listPrevious comme étant des ArrayList
+     */
     public Node() {
-//        TODO
-    }
-
-    protected void setiIdNode(int iIdNode) {
-        this.iIdNode = iIdNode;
-    }
-
-    protected void setNextNode(int iNumNext) {
-//        TODO
-    }
-
-    protected void setPreviousNode(int iNumNext) {
-//        TODO
-    }
-
-    protected List<Integer> getCopyListPrevious() {
-//        TODO
-    }
-
-    public List<Integer> getCopyListNexts() {
-//        TODO
-    }
-
-    public List<Integer> getListNexts() {
-        return this.listNexts;
+        this.listNexts = new ArrayList<>();
+        this.listPrevious = new ArrayList<>();
     }
 
     public int compareTo(Node o) {
 //        TODO
+        return 0;
     }
 
-    public int getNbNexts() {
-//        TODO
+    /**
+     * Permet de faire un copy de listPrevious
+     * @return : référence vers une copy de listPrevious de l'objet courant
+     */
+    protected List<Integer> getCopyListPrevious() {
+        return new ArrayList<>(this.listPrevious);
     }
 
-    public int getNbPrevious() {
-//        TODO
+    /**
+     * Permet de faire un copy de listNexts
+     * @return : référence vers une copy de listNexts de l'objet courant
+     */
+    public List<Integer> getCopyListNexts() {
+        return new ArrayList<>(this.listNexts);
     }
 
+    /**
+     * Permet de renseigner la valeur de iIdNode
+     * @param iIdNode : valeur qu'on souhaite affecter à iIdNode sur l'objet courant
+     */
+    protected void setiIdNode(int iIdNode) {
+        this.iIdNode = iIdNode;
+    }
+
+    /**
+     * Permet d'ajouter une valeur dans listNexts
+     * @param iNumNext : valeur qu'on souhaite ajouter à listNexts
+     */
+    protected void setNextNode(int iNumNext) {
+        this.listNexts.add(iNumNext);
+    }
+
+    /**
+     * Permet d'ajouter une valeur dans listPrevious
+     * @param iNumNext : valeur qu'on souhaite ajouter à listPrevious
+     */
+    protected void setPreviousNode(int iNumNext) {
+        this.listPrevious.add(iNumNext);
+    }
+
+    /**
+     * @return : taille de listNexts sur l'objet courant
+     */
+    public List<Integer> getListNexts() {
+        return this.listNexts;
+    }
+
+    /**
+     * Permet d'obtenir une valeur de listNexts a l'index passé en paramètre
+     * @param i : valeur de l'index
+     * @return : valeur de listNexts à l'index passé en paramètre sur l'objet courant
+     */
     public int getNext(int i) {
-//        TODO
+        return this.listNexts.get(i);
     }
 
+    /**
+     * @return : taille de listNexts sur l'objet courant
+     */
+    public int getNbNexts() {
+        return this.listNexts.size();
+    }
+
+    /**
+     * @return : taille de listPrevious sur l'objet courant
+     */
+    public int getNbPrevious() {
+        return this.listPrevious.size();
+    }
+
+    /**
+     * @return : valeur de iIdNode sur l'objet courant
+     */
     public int getiIdNode() {
         return this.iIdNode;
     }
