6e182e0bd7b90f83097c01b1a67f53be797bd161
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-05-27 22:28:51 +0200

    [IMP] taskNode class

--- src/model/problem/TaskNode.java
+++ src/model/problem/TaskNode.java
@@ -1,46 +1,78 @@
 package model.problem;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Classe TaskNode
+ * @author : Alexis V.
+ * @version : 1.0
+ */
 public class TaskNode extends Node {
 
-    protected List<Alternative> listAlternatives;
     protected int iIdTask;
     protected String sName;
+    protected List<Alternative> listAlternatives;
 
+    /**
+     * TODO
+     * @param sc
+     * @param gp
+     */
     public TaskNode(Scanner sc, GraphProject gp) {
-//        TODO
+        this.sName = sc.next();
+        this.iIdTask = sc.nextInt();
+        this.listAlternatives = new ArrayList<>();
+        for (int i = 0; i < sc.nextInt(); i++) {
+            this.listAlternatives.add(new Alternative(sc, gp));
+        }
     }
 
     public String toString() {
 //        TODO
+        return null;
     }
 
+    /**
+     * Permet de récupérer le cout de l'alternative a un un index donné
+     * @param iSelectedTaskNodeAlternative : index dans la liste d'alternative ou on souhaite récupérer son cout
+     * @return : cout de l'alternative
+     */
     public double getCost(int iSelectedTaskNodeAlternative) {
-//        TODO
+        return this.listAlternatives.get(iSelectedTaskNodeAlternative).getCost();
     }
 
+    /**
+     * Permet de récupérer la duree de l'alternative a un un index donné
+     * @param iSelectedTaskNodeAlternative : index dans la liste ou on souhaite récupérer sa duree
+     * @return : duree de l'alternative
+     */
     public double getDuration(int iSelectedTaskNodeAlternative) {
-//        TODO
+        return this.listAlternatives.get(iSelectedTaskNodeAlternative).getDuration();
     }
 
     public Boolean isResourcesAvailable(List<Resource> listAvailableResources, int iSelectedTaskNodeAlternative) {
 //        TODO
+        return false;
     }
 
     public boolean isResourcesAvailable(List<Resource> listAvailableResources, boolean bIsMinCalculated, int iNumObjectif) {
 //        TODO
+        return false;
     }
 
     public List<Resource> getListResourcesofSelectedAlternative(int iSelectedTaskNodeAlternative) {
 //        TODO
+        return null;
     }
 
     public Alternative getAlternativeExtremum(boolean bIsMinCalculated, int iNumObjectif) {
 //        TODO
+        return null;
     }
+
 
     protected void setiIdTask(int iIdTask) {
         this.iIdTask = iIdTask;
@@ -48,6 +80,7 @@ public class TaskNode extends Node {
 
     public int getNbAlternatives() {
 //        TODO
+        return 0;
     }
 
     protected int getiIdTask() {
@@ -55,14 +88,26 @@ public class TaskNode extends Node {
     }
 }
 
+/**
+ * Classe ComparatorAlternativeDuration
+ * @author : Alexis V.
+ * @version : 1.0
+ */
 class ComparatorAlternativeDuration implements Comparator<Alternative> {
     public int compare(Alternative alt1, Alternative alt2) {
 //        TODO
+        return 0;
     }
 }
 
+/**
+ * Classe ComparatorAlternativeCost
+ * @author : Alexis V.
+ * @version : 1.0
+ */
 class ComparatorAlternativeCost implements Comparator<Alternative> {
     public int compare(Alternative alt1, Alternative alt2) {
 //        TODO
+        return 0;
     }
 }
