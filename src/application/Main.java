b2f70d25d1baca59f72ef6f76f2c3b8b97ba7e18
Inès LEGROS <ROSELILAVAL1@gmail.com>
2020-05-31 22:25:58 +0200

    commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,7 +8,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
-public class Main extends Application {
+public class Main extends Application 
+{
 	@Override
 	public void start(Stage primaryStage) {
 		try {
