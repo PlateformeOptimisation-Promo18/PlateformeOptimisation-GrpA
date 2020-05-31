ebcd7fe12deb83c755893e037ac95faa303377cd
Jérôme Bioulac <jerome@bioulac.fr>
2020-05-31 15:01:07 +0200

    implemetation méthode compareTo

--- src/model/problem/RessourcesPlanned.java
+++ src/model/problem/RessourcesPlanned.java
@@ -1,5 +1,33 @@
 package model.problem;
 
-public class RessourcesPlanned {
+import java.util.List;
 
+public class RessourcesPlanned implements Comparable<RessourcesPlanned> {
+
+    protected double dReleaseDate;
+    protected List<Resource> listResource;
+
+    public RessourcesPlanned(List<Resource> listResource, double dReleaseDate) {
+        this.dReleaseDate = dReleaseDate;
+        this.listResource = listResource;
+    }
+
+    protected double getdReleaseDate() {
+        return this.dReleaseDate;
+    }
+
+    protected List<Resource> getListResource() {
+        return this.listResource;
+    }
+
+    @Override
+    public int compareTo(RessourcesPlanned ressourcesPlanned) {
+        if (ressourcesPlanned.dReleaseDate < this.dReleaseDate) {
+            return 1;
+        } else if (ressourcesPlanned.dReleaseDate > this.dReleaseDate) {
+            return 0;
+        } else {
+            return 0;
+        }
+    }
 }
