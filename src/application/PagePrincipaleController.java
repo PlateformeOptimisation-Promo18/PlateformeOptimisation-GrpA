6a89523c33263fb3eb86d5fdc527546ef4ebb1e1
Inès LEGROS <ROSELILAVAL1@gmail.com>
2020-05-31 23:59:10 +0200

    Modification des tab de la page principale. paul et zied n'ont plus qu'à décommenter leur ligne dans pageprincipalecontroller pour que leur page s'affiche.

--- src/application/PagePrincipaleController.java
+++ src/application/PagePrincipaleController.java
@@ -1,19 +1,13 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
-import javafx.scene.SubScene;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
 
 public class PagePrincipaleController 
 {
-	@FXML
-	private SubScene pageOptimisation;
-	
-	@FXML
-	private SubScene pageComparaison;
-	
-	@FXML
-	private SubScene pageConsultation;
 
 	@FXML
 	private Tab fenetreOptimisation;
@@ -23,9 +17,28 @@ public class PagePrincipaleController
 	
 	@FXML
 	private Tab fenetreConsultation;
-	
-	
-	
+
+
+	@FXML
+	public void initialize ()
+	{
+		try
+        {
+        	fenetreConsultation.setContent(FXMLLoader.load(getClass().getResource("Demarage.fxml")));
+        	
+        	//a decommenter pour zied
+        	//fenetreComparaison.setContent(FXMLLoader.load(getClass().getResource("Comparaison.fxml")));
+        	
+        	//a decommenter pour paul
+        	//fenetreOptimisation.setContent(FXMLLoader.load(getClass().getResource("Optimisation.fxml")));
+        	
+        }
+        catch(IOException iex)
+        {
+            System.out.println("unable to load OptimmisationTab");
+        }
+		
+	}
 	
 	
 	
