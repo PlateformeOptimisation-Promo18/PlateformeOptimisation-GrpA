81abe17876dc3705d5a94eb7f838c664c4a7e758
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-06-01 11:42:11 +0200

    [FIX] missing code

--- src/model/problem/GraphProject.java
+++ src/model/problem/GraphProject.java
@@ -1,14 +1,18 @@
 package model.problem;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.*;
 
 import model.generic.Problem;
 import model.generic.Solution;
 
+
+/**
+ * Classe Resource
+ * @author : Alexis V. / Jean L. / Mathys P.
+ * @version : 1.0
+ */
 public class GraphProject implements Problem {
 
     private String sName;
@@ -54,37 +58,52 @@ public class GraphProject implements Problem {
             sc.next();
             this.iNbTaskNode = sc.nextInt();
             for (int i = 0; i < this.iNbTaskNode; i++) {
-                this.projectGraph.add(new TaskNode(sc, this));
+                TaskNode task = new TaskNode(sc, this);
+                task.setiIdNode(i);
+                this.projectGraph.add(task);
             }
 
-            // Chargement des Noeux OU
+            // Chargement des Noeuds OU
             // on "supprime" l'information "nombre_de_choix"
             sc.next();
             this.iNbOrNode = sc.nextInt();
             for (int i = 0; i < this.iNbOrNode; i++) {
-                this.projectGraph.add(new OrNode(sc));
+                OrNode or = new OrNode(sc);
+                EndOrNode endOr = new EndOrNode(sc);
+                or.setiIdEndOrNode(endOr.getiIdNode());
+                endOr.setiIdBeginOrNode(or.getiIdNode());
+                this.projectGraph.add(or);
+                this.projectGraph.add(endOr);
             }
 
-            // Chargement des Noeux ET
+            // Chargement des Noeuds ET
             // on "supprime" l'information "nombre_de_ands"
             sc.next();
             this.iNbAndNode = sc.nextInt();
             for (int i = 0; i < this.iNbAndNode; i++) {
-                this.projectGraph.add(new AndNode(sc));
+                AndNode and = new AndNode(sc);
+                EndAndNode endAnd = new EndAndNode(sc);
+                and.setiIdEndAndNode(endAnd.getiIdNode());
+                endAnd.setiIdBeginAndNode(and.getiIdNode());
+                this.projectGraph.add(and);
+                this.projectGraph.add(endAnd);
             }
 
-            // On trie le graph de projet avant l'ajout des noeux
+            // On trie le graph de projet avant l'ajout des Noeuds
             Collections.sort(projectGraph);
 
-            // Chargement des Noeux
+            // Chargement des Noeuds
             // on "supprime" l'information "nombre_de_sommets"
             sc.next();
             int iNbNode = sc.nextInt();
             for (int i = 0; i < iNbNode; i++) {
                 Node node = this.projectGraph.get(sc.nextInt());
-                int iNextNode = sc.nextInt();
-                for (int j = 0; j < iNextNode; j++) {
-                    node.setNextNode(sc.nextInt());
+                int iNbNext = sc.nextInt();
+                for (int j = 0; j < iNbNext; j++) {
+                    int iNext = sc.nextInt();
+                    node.setNextNode(iNext);
+                    Node nextNode = this.projectGraph.get(iNext);
+                    nextNode.setPreviousNode(this.iNbOrNode + node.getiIdNode());
                 }
             }
 
