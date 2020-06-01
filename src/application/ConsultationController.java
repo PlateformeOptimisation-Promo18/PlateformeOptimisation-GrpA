e9b9e6533497caa1ab8373ffc92c86c93339cd85
Inès LEGROS <ROSELILAVAL1@gmail.com>
2020-06-01 02:51:55 +0200

    Page consultationController terminée à améliorer

--- src/application/ConsultationController.java
+++ src/application/ConsultationController.java
@@ -1,12 +1,17 @@
 package application;
 
 import java.awt.Desktop;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -21,31 +26,51 @@ public class ConsultationController
 	private Button boutonUpload;
 	
 	@FXML
-	private LineChart visualisationChart;
+	private LineChart<Number, Number>  visualisationChart;
 	
 	@FXML
 	private void boutonUploadClick (ActionEvent evt)
 	{		
-		Stage primaryStage = (Stage) boutonUpload.getScene().getWindow();
-		
-		final FileChooser fileChooser = new FileChooser();
-		
-		File file = fileChooser.showOpenDialog(primaryStage);
-		
-		try {
-			Scanner SC = new Scanner(file);
 
-            while (SC.hasNextDouble()) 
-            {
-            	Double x = SC.nextDouble();
-            	Double y = SC.nextDouble();
-				visualisationChart.getData().add(new XYChart.Data(x, y));
-            }
-            SC.close();
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+		// Name axes
+		visualisationChart.getXAxis().setLabel("Hypervolum");
+		visualisationChart.getYAxis().setLabel("Time");  
+		visualisationChart.setTitle("Visualisation");
+
+		// Definion des series
+		XYChart.Series series = new XYChart.Series();
+		
+		FileChooser fc = new FileChooser() ;
+		fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt"));
+		File selectedFile = fc.showOpenDialog(null) ;
+		
+		try 
+		{
+			
+			if ( selectedFile != null) 
+			{
+				FileReader filereader = new FileReader(selectedFile.getPath());
+				BufferedReader bufferedreader = new BufferedReader(filereader);
+
+				String ligne;
+				int compteur = 0;
+				
+				while ((ligne = bufferedreader.readLine()) != null) 
+				{
+					if (compteur > 0) 
+					{
+						series.getData().add(new XYChart.Data(Double.parseDouble(ligne.substring(14,21)),Double.parseDouble(ligne.substring(0,13)) ));
+						
+					}
+					
+					compteur++;
+				}
+				visualisationChart.getData().add(series);
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -57,5 +82,6 @@ public class ConsultationController
 		imageView.setFitHeight(50);
 		imageView.setFitWidth(50);
 		boutonUpload.setGraphic(imageView);
+		
 	}
 }
